08c0b4047be6ed38b1cdf95dabf5eef2176fa205
Haidir Aditya <haidiraditya16@gmail.com>
2025-02-21 18:56:31 +0800

    membuat tombol jadi lebih modular dan membatalkan pembuatan IceLibrary (burik bjir), jadi kita akan menggunakan model toko pada umumnya saja, tapi efek tombolnya masih kenyal (masih dalam tahap pengembangan)

---
+++ src/components/LaptopView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./laptop/Header";
+
+function LaptopView() {
+  return (
+    <div className="font-capriola">
+      <Header />
+    </div>
+  );
+}
+
+export default LaptopView;
